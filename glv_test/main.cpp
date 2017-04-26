019d511e4b6e2240e383ce857d319c3d03f4ad9b
Keehong Youn <younkeehong@gmail.com>
2017-04-26 01:34:59 -0700

    wip slider working

--- glv_test/main.cpp
+++ glv_test/main.cpp
@@ -22,32 +22,18 @@ public:
   glv::GLV glv;
   glv::Slider sl1 {glv::Rect(120,20,301,31)};
   glv::Slider sl2 {glv::Rect(120,60,200,50)};
+  float var = 1; // !!!
 
 void onCreate() {
     shader.compile(al_default_vert_shader(), al_default_frag_shader());
     
     auto show = [](auto const& a) { std::cout << a << std::endl; };
     
-    float var = 1;
     
     sl1.attachVariable(var);
     sl2.attachVariable(var);
     
     glv << sl1 << sl2;
-
-    // show((glv.child == &sl1));
-    // show((glv.child == &sl2));
-    // show((sl1.sibling == &sl2));
-    // show((sl2.parent == &glv));
-
-    // show("sl1.size(): " + std::to_string(sl1.size()));
-    // show(sl1.getValue());
-    // show(var);
-    // sl1.select(0);
-    // sl1.setValue(0.5);
-    // show(sl1.getValue());
-    // show(var);
-
   }
 
   void onAnimate(double dt) {
