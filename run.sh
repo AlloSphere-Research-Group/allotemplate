d342c7158b602357981f1b27dc264989c1f60841
Keehong Youn <younkeehong@gmail.com>
2017-07-23 01:27:20 -0700

    make run script callable from anywhere

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+INITIALDIR=${PWD}
+# echo "Script executed from: ${INITIALDIR}"
+
+# BASH_SOURCE has the script's path
+# could be absolute, could be relative
+SCRIPT_PATH=$(dirname ${BASH_SOURCE})
+
+FIRSTCHAR=${SCRIPT_PATH:0:1}
+if [ ${FIRSTCHAR} == "/" ]; then
+  # it's asolute path
+  AL_PROJ_PATH=${SCRIPT_PATH}
+else
+  # SCRIPT_PATH was relative
+  AL_PROJ_PATH=${INITIALDIR}/${SCRIPT_PATH}
+fi
+
 # forward to al_lib run script
-./al_lib/run.sh $@
+${AL_PROJ_PATH}/al_lib/run.sh $@
