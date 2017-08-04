9016e0a601c96c9755ff802dcf7a107b54717875
Keehong Youn <younkeehong@gmail.com>
2017-08-03 17:38:06 -0700

    update allolib commit & simplify run script

--- run.sh
+++ run.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-INITIALDIR=${PWD}
-# echo "Script executed from: ${INITIALDIR}"
-
 # BASH_SOURCE has the script's path
-# could be absolute, could be relative
 SCRIPT_PATH=$(dirname ${BASH_SOURCE})
-
-FIRSTCHAR=${SCRIPT_PATH:0:1}
-if [ ${FIRSTCHAR} == "/" ]; then
-  # it's asolute path
-  AL_PROJ_PATH=${SCRIPT_PATH}
-else
-  # SCRIPT_PATH was relative
-  AL_PROJ_PATH=${INITIALDIR}/${SCRIPT_PATH}
-fi
-
 # forward to allolib run script
-${AL_PROJ_PATH}/allolib/run.sh $@
+${SCRIPT_PATH}/allolib/run.sh $@
