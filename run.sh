ab6a4c3cf20aa63596a0fa56152dfad0e3aeb487
Keehong Youn <younkeehong@gmail.com>
2018-02-01 17:23:08 -0800

    add -v flag for run script

--- run.sh
+++ run.sh
@@ -24,42 +24,52 @@ fi
 # resolve flags
 BUILD_TYPE=Release # release build by default
 DO_CLEAN=0
-while getopts ":dnc" opt; do
-  case $opt in
+IS_VERBOSE=0
+VERBOSE_FLAG=OFF
+
+while getopts "dncv" opt; do
+  case "${opt}" in
   d)
-  BUILD_TYPE=Debug
-  POSTFIX=_debug # if release, there's no postfix
-  shift # consume option
+    BUILD_TYPE=Debug
+    POSTFIX=_debug # if release, there's no postfix
     ;;
   n)
-  EXIT_AFTER_BUILD=1
-  shift
+    EXIT_AFTER_BUILD=1
     ;;
   c)
-  DO_CLEAN=1
-  shift
+    DO_CLEAN=1
+    ;;
+  v)
+    IS_VERBOSE=1
+    VERBOSE_FLAG=ON
     ;;
   esac
 done
-echo "BUILD TYPE: ${BUILD_TYPE}"
+# consume options that were parsed
+shift $(expr $OPTIND - 1 )
+
+if [ ${IS_VERBOSE} == 1 ]; then
+  echo "BUILD TYPE: ${BUILD_TYPE}"
+fi
 
 # build allolib
 echo " "
 echo "___ building allolib __________"
-echo " "
 
 cd ${AL_LIB_PATH}
 git submodule init
 git submodule update
 if [ ${DO_CLEAN} == 1 ]; then
-  echo "cleaning build"
+  if [ ${IS_VERBOSE} == 1 ]; then
+    echo "cleaning build"
+  fi
   rm -r build
 fi
 mkdir -p build
 cd build
 mkdir -p "${BUILD_TYPE}"
 cd "${BUILD_TYPE}"
-cmake ../.. -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DAL_VERBOSE_OUTPUT=${VERBOSE_FLAG} ../..
 make
 LIB_BUILD_RESULT=$?
 if [ ${LIB_BUILD_RESULT} != 0 ]; then
@@ -72,7 +82,6 @@ cd ${AL_TEMPLATE_PATH}
 if [ -d "Gamma" ]; then
   echo " "
   echo "___ Gamma found, building Gamma __________"
-  echo " "
   cd Gamma
   mkdir -p build
   cd build
@@ -97,7 +106,6 @@ APP_FILE=$(basename ${APP_FILE_INPUT})
 APP_NAME=${APP_FILE%.*} # remove extension (once, assuming .cpp)
 echo " "
 echo "___ building ${APP_NAME} __________"
-echo " "
 # echo "    app path: ${APP_PATH}"
 # echo "    app file: ${APP_FILE}"
 # echo "    app name: ${APP_NAME}"
@@ -108,14 +116,16 @@ echo " "
 cd ${INITIALDIR}
 cd ${APP_PATH}
 if [ ${DO_CLEAN} == 1 ]; then
-  echo "cleaning build"
+  if [ ${IS_VERBOSE} == 1 ]; then
+    echo "cleaning build"
+  fi
   rm -r build
 fi
 mkdir -p build
 cd build
 mkdir -p ${APP_NAME}
 cd ${APP_NAME}
-cmake ${AL_LIB_PATH}/cmake/single_file -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -Dal_path=${AL_LIB_PATH} -DAL_APP_FILE=../../${APP_FILE} -Dapp_include_dirs=${GAMMA_INCLUDE_DIRS}\; -Dapp_link_libs=${GAMMA_LINK_LIBS}\;
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -Dal_path=${AL_LIB_PATH} -DAL_APP_FILE=../../${APP_FILE} -Dapp_include_dirs=${GAMMA_INCLUDE_DIRS}\; -Dapp_link_libs=${GAMMA_LINK_LIBS}\; -DAL_VERBOSE_OUTPUT=${VERBOSE_FLAG} ${AL_LIB_PATH}/cmake/single_file
 make
 
 APP_BUILD_RESULT=$?
@@ -134,5 +144,4 @@ cd ${INITIALDIR}
 cd ${APP_PATH}/bin
 echo " "
 echo "___ running ${APP_NAME} __________"
-echo " "
 ./"${APP_NAME}${POSTFIX}"
