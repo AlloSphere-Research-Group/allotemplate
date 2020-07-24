1dc174ca070f16bb192c8515b9b59c2c80a87b02
Kon Hyong Kim <konhyong@gmail.com>
2020-07-24 14:17:56 -0700

    updated change in polygonLine()

--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,20 @@ struct MyApp : App {
   void onCreate() override {
     addTetrahedron(mesh);
     lens().near(0.1).far(25).fovy(45);
-    nav().pos(0,0,4);
-    nav().quat().fromAxisAngle(0.*M_2PI, 0,1,0);
+    nav().pos(0, 0, 4);
+    nav().quat().fromAxisAngle(0. * M_2PI, 0, 1, 0);
   }
 
   void onAnimate(double dt) override {
     double period = 10;
     phase += dt / period;
-    if(phase >= 1.) phase -= 1.;
+    if (phase >= 1.) phase -= 1.;
   }
 
   void onDraw(Graphics& g) override {
     g.clear(0, 0, 0);
-    g.polygonMode(Graphics::LINE);
-    g.pushMatrix();
-    g.rotate(phase*360, 0,1,0);
+    g.polygonLine();
+    g.rotate(phase * 360, 0, 1, 0);
     g.color(1);
     g.draw(mesh);
     g.popMatrix();
