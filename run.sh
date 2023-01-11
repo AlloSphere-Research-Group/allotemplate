014986fb485342d08bea46896c20d8c0d6d9a8ef
Kon Hyong Kim <konhyong@gmail.com>
2023-01-11 03:50:08 -0800

    run.sh explicitly uses release config

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
   # utilizing cmake's parallel build options
   # for cmake >= 3.12: -j <number of processor cores + 1>
   # for older cmake: -- -j5
-  cmake --build build/release -j 9
+  cmake --build build/release --config Release -j 9
 )
 
 result=$?
