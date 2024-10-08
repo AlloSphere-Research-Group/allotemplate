cc35636a6748a9e7256220c9ddf4405d2e71985c
Kon Hyong Kim <konhyong@gmail.com>
2024-10-08 14:41:49 -0700

    change executable name to run

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 cmake --build build/release --config Release --parallel %NUMBER_OF_PROCESSORS% -- /p:CL_MPcount=%NUMBER_OF_PROCESSORS%
 @REM cmake --build build/release --config Debug --parallel %NUMBER_OF_PROCESSORS% -- /p:CL_MPcount=%NUMBER_OF_PROCESSORS%
 
-if %ERRORLEVEL% EQU 0 cd bin & .\projector-control
+if %ERRORLEVEL% EQU 0 cd bin & .\app_name_here
