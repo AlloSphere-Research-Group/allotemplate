cd340b9eb0878e77ba0a13d815bd204af125012a
Kon Hyong Kim <konhyong@gmail.com>
2025-10-24 11:49:55 -0700

    updated allolib and al_ext

--- configure.bat
+++ configure.bat
@@ -1,3 +1,2 @@
-@REM cmake -DCMAKE_BUILD_TYPE=RELEASE -B build/Release -S .
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -B build/Release -S .
-@REM cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -Wno-deprecated -DBUILD_EXAMPLES=0 -B build/Release -S .
+@echo off
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DALLOLIB_ADD_EXAMPLES=0 -B build/release -S .
--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
 
-cmake --build build/Release --config Release -j %NUMBER_OF_PROCESSORS% -- /p:CL_MPCount=3
-@REM cmake --build build/Debug --config Debug -j %NUMBER_OF_PROCESSORS% -- /p:CL_MPCount=3
+cmake --build build/release --config Release --parallel %NUMBER_OF_PROCESSORS% -- /p:CL_MPcount=3
 
-if %ERRORLEVEL% EQU 0 cd bin & .\app_name_here
+if %ERRORLEVEL% EQU 0 cd bin & .\arena
