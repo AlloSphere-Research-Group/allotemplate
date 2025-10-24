fd1fd59a50461a5412f77883575f3e0f62201927
Kon Hyong Kim <konhyong@gmail.com>
2025-10-24 13:46:15 -0700

    changed name of binary

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 
 cmake --build build/release --config Release --parallel %NUMBER_OF_PROCESSORS% -- /p:CL_MPcount=3
 
-if %ERRORLEVEL% EQU 0 cd bin & .\arena
+if %ERRORLEVEL% EQU 0 cd bin & .\app_name_here
